c808c7618df749b342abcfd2b3d4392c0f1079d3
Aliaksandr Hokk <external.aliaksandr.hokk@kuehne-nagel.com>
2024-12-02 12:20:10 +0100

    add test users to DB

---
+++ sleep/src/main/resources/db/migration/V1.2.1__test_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO user_entity (id, time_zone) VALUES (101, 'Europe/Warsaw');
+INSERT INTO user_entity (id, time_zone) VALUES (102, 'UTC');
+INSERT INTO user_entity (id, time_zone) VALUES (103, '-05:00');
+INSERT INTO user_entity (id, time_zone) VALUES (104, 'UTC');
+INSERT INTO user_entity (id, time_zone) VALUES (105, 'Europe/Warsaw');
